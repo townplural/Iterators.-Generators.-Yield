dd912ac43d9f3fae4bbbbc12702879df969d3d0a
zitraxmode <bobrenskix.bogdan@gmail.com>
2023-08-24 18:13:05 +0300

    flat_generator.py test

--- flat_generator.py
+++ flat_generator.py
@@ -2,31 +2,28 @@ import types
 
 
 def flat_generator(list_of_lists):
-    ...
-    yield
-    ...
-
-    # первая итерация: Код до yield, yield
-    # следующие итерации: код после yield, код до yield, yield
+    for inner_list in list_of_lists:
+        for value in inner_list:
+            yield value
 
 
-# def test_2():
-#     list_of_lists_1 = [
-#         ['a', 'b', 'c'],
-#         ['d', 'e', 'f', 'h', False],
-#         [1, 2, None]
-#     ]
-#
-#     for flat_iterator_item, check_item in zip(
-#             flat_generator(list_of_lists_1),
-#             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-#     ):
-#         assert flat_iterator_item == check_item
-#
-#     assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-#
-#     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
-#
-#
-# if __name__ == '__main__':
-#     test_2()
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
